33ef070f2dbb6d415c117c952b0b1cb88ed6d4c6
이동훈 <62137001+ldh019@users.noreply.github.com>
2022-09-22 08:59:49 +0900

    Update 9024_동훈.cpp

--- week1/9024_동훈.cpp
+++ week1/9024_동훈.cpp
@@ -1,14 +1,40 @@
 #include <iostream>
-#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
 
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
-    while (n--) {
-        string input;
-        cin >> input;
-        cout << input[0] << input[input.size() - 1] << "\n";
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++)
+            cin >> v[i];
+        sort(v.begin(), v.end());
+
+        int f = 0, r = n - 1;
+        int ans = v[f] + v[r];
+        int cnt = 0;
+        while (f < r) {
+            int tmp = v[f] + v[r];
+            if (abs(ans - k) > abs(tmp - k)) {
+                ans = tmp;
+                cnt = 1;
+            } else if (abs(ans - k) == abs(tmp - k)) {
+                cnt++;
+            }
+            if (tmp <= k) f++;
+            else r--;
+        }
+        cout << cnt << "\n";
     }
+    return 0;
 }

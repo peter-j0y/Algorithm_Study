14d4b0e3098e47a13fa12e91191be3d4c7489267
Seungmin-develop <tmdals8798@gmail.com>
2023-05-13 23:11:53 +0900

    0513 승민
    https://www.acmicpc.net/problem/14889

---
+++ 2023/5월 2주차/14889_승민.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(now, count):
+    global answer
+    if count == n//2:
+        team_a, team_b = 0, 0
+        for i in range(n):
+            for j in range(n):
+                if visited[i] and visited[j]:
+                    team_a += graph[i][j]
+                elif not visited[i] and not visited[j]:
+                    team_b += graph[i][j]
+
+        answer = min(answer, abs(team_a-team_b))
+    
+    for i in range(now, n):
+        if visited[i]:
+            continue
+        visited[i] = 1
+        dfs(now+1, count+1)
+        visited[i] = 0
+
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
+visited = [0 for _ in range(n)]
+answer = sys.maxsize
+
+dfs(0, 0)
+print(answer)
